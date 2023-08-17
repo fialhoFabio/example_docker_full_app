8c97a4888e3d43fd936689a3dd45a557941eab49
fialhoFabio <fabiom456@gmail.com>
2023-08-17 09:59:16 -0300

    😒

--- api/controllers/ApiController.php
+++ api/controllers/ApiController.php
@@ -13,8 +13,8 @@ class ApiController extends Controller
 	public function actionIndex()
 	{
 		\Yii::$app->response->format = Response::FORMAT_JSON;
-		return Pais::find()->all();
-//		return ['message' => 'Minha primeira API com Yii2'];
+//		return Pais::find()->all();
+		return ['message' => 'Cuuuuuuuuuuuu'];
 	}
 
 	// Mais ações da sua API podem ser adicionadas aqui
